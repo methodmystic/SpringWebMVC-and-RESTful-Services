8a06b8cb43bfd5f00b5ace65b14e18e899d195db
methodmystic <pranavshinde9906@gmail.com>
2026-08-04 18:38:58 +0530

    web 01

---
+++ Web-App-01/src/main/java/com/telusko/WebApp01Application.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApp01Application.class, args);
+	}
+
+}
